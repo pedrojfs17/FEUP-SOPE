28e9f8e3baacecc4f94a87427db619609546c153
pedrojfs17 <up201806227@fe.up.pt>
2020-03-10 14:38:54 +0000

    Finished p8

---
+++ TP3/p8c.c
@@ -0,0 +1,28 @@
+// PROGRAMA p8.c
+#include <stdio.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[], char *envp[])
+{
+    pid_t pid;
+
+    char *command[] = {"ls","-la",NULL};
+
+    if (argc != 2) {
+        printf("usage: %s dirname\n",argv[0]);
+        exit(1);
+    }
+
+    pid=fork();
+    if (pid > 0)
+        printf("My child is going to execute command \"ls -laR %s\"\n", argv[1]);
+    else if (pid == 0){
+        execvp("ls",command);
+        printf("Command not executed !\n");
+        exit(1);
+    }
+    
+    exit(0);
+} 
