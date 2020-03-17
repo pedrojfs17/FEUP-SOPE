693d62661fc245724a422181ca252779df505442
pedrojfs17 <up201806227@fe.up.pt>
2020-03-17 14:29:41 +0000

    Finished p2

--- TP4/p2a.c
+++ TP4/p2a.c
@@ -1,30 +1,27 @@
-// PROGRAMA p01a.c
+// PROGRAMA p02a.c
 #include <stdio.h>
 #include <signal.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <stdlib.h> 
 
 void sigint_handler(int signo) {
-    printf("In SIGINT handler ...\n");
-}
+    printf("Entering SIGINT handler ...\n");
+    sleep(10);
+    printf("Exiting SIGINT handler ...\n");
+} 
 
 int main(void) {
     struct sigaction action;
     action.sa_handler = sigint_handler;
     sigemptyset(&action.sa_mask);
     action.sa_flags = 0;
-
+    
     if (sigaction(SIGINT,&action,NULL) < 0) {
         fprintf(stderr,"Unable to install SIGINT handler\n");
         exit(1);
     }
-    
-    printf("Sleeping for 30 seconds ...\n");
 
-    int time_left=sleep(30);
-
-    while(time_left!=0){time_left=sleep(time_left);}
-    
-    printf("Waking up ...\n");
+    printf("Try me with CTRL-C ...\n");
+    while(1) pause();
     exit(0);
 } 
