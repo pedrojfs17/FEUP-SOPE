c35208086ef0a1ea14c365bfd088fd1025cbebcd
pedrojfs17 <up201806227@fe.up.pt>
2020-03-23 18:48:46 +0000

    Added args struct

---
+++ Projects/FirstProject/args.h
@@ -0,0 +1,9 @@
+struct Args {
+    int all;
+    int bytes;
+    int block_size;
+    int countLinks;
+    int deference;
+    int separateDirs;
+    int max_depth;
+};
--- Projects/FirstProject/simpledu.c
+++ Projects/FirstProject/simpledu.c
@@ -7,24 +7,26 @@
 #include <sys/stat.h>
 #include <fcntl.h>
 
+#include "args.h"
+
 #define COMMAND_SIZE 200
 #define MAX_NUM_COMMANDS 10
 #define ARGS (const char*[14]){"-a", "--all", "-b", "--bytes", "-B", "--block-size=", "-l", "--count-links", "-L", "--deference", "-S", "--separate-dirs", "--max-depth="}
-#define ARGS_SIZE
+#define ARGS_SIZE 14
 
 // Check if 'element' is in 'arr' of size 'arr_size'
-bool check_in_array(char *arr[], int arr_size, char *element) {
-    for(int i = 0; i < len; ++i) {
+int check_in_array(char *arr[], int arr_size, char *element) {
+    for(int i = 0; i < arr_size; ++i) {
         if (!strcmp(arr[i], element))
-            return true;
+            return 1;
     }
-    return false;
+    return 0;
 }
 
 // Check if a path is valid (return -1), a directory (return 1) or a file (return 0)
 int check_path(const char *path) {
     struct stat path_stat;
-    if (stat(path, &myFpath_statile) < 0) // Invalid path
+    if (stat(path, &path_stat) < 0) // Invalid path
         return -1;
     else if (S_ISDIR(path_stat.st_mode)) // Directory
         return 1;
@@ -35,34 +37,36 @@ int check_path(const char *path) {
 }
 
 // Check if the arguments are valid
-bool check_args(int argc, char *argv[]) {
-    bool path_found = false;
+int check_args(int argc, char *argv[]) {
+    int path_found = 0;
     char *temp[14];
     int temp_size = 0;
 
     for (int i = 1; i < argc; i++) {
-        if (check_in_array(ARGS, ARGS_SIZE, argv[i])) {
-            if (!check_in_array(temp, temp_size, argv[i])) {
+        if (check_in_array(ARGS, ARGS_SIZE, argv[i])) { // Check valid Argument
+            if (!check_in_array(temp, temp_size, argv[i])) { // Check if it isn't duplicate
                 temp[temp_size] = argv[i];
                 temp_size++;
             }
-            else // Duplicate argument
-                return false;
+            else
+                return 0;
         }
         else {
             if (!path_found && check_path(argv[i]) >= 0)
-                path_found = true;
+                path_found = 1;
             else
-                return false;
+                return 0;
         }
     }
 
-    return true;
+    return 1;
 }
 
 int main(int argc, char *argv[], char *envp[])
 {
-    if (argc > MAX_NUM_COMMANDS || !check_args(argv)) {
+    struct Args args = {0, 0, 1024, 0, 0, 0, -1};
+
+    if (argc > MAX_NUM_COMMANDS || !check_args(argc, argv)) {
         fprintf(stderr, "Usage: %s -l [path] [-a] [-b] [-B size] [-L] [-S] [--max-depth=N]\n", argv[0]);
         exit(1);
     }
