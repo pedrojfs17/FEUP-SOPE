a6f6159de5901a652cf3b8e22e478be732c26d26
g-batalhao-a <al.darkseid.2010@gmail.com>
2020-04-22 15:07:55 +0100

    Started working on time condition in Client

--- Projects/SecondProject/U1.c
+++ Projects/SecondProject/U1.c
@@ -7,6 +7,9 @@
 #include <signal.h>
 #include <errno.h>
 #include <string.h>
+#include <sys/time.h>
+#include <pthread.h>
+
 #include "defs.h"
 
 #define MAX_MSG_LEN 255
@@ -15,15 +18,19 @@
 int i=0;
 
 void *threader(void * arg){
-    int fd=open(*(char**)arg|O_WRONLY);
-
+    int fd=open(*(char**)arg,O_WRONLY);
+    int fd_dummy;
     if(fd==-1){
         printf("Oops !!! Service is closed !!!\n");
+        pthread_exit(NULL);
     }
     printf("FIFO '%s' openned in WRITEONLY mode\n",*(char**)arg);
 
     char msg[MAX_MSG_LEN];
-    sprintf(msg,"[ %d, %d, %d, %d, -1]",i,getpid(),pthread_self(),rand()%20+1);
+    time_t t;
+    srand((unsigned)time(&t));
+
+    sprintf(msg,"[ %d, %d, %ld, %d, -1]",i,getpid(),pthread_self(),rand()%20+1);
     write(fd,&msg,MAX_MSG_LEN);
     close(fd);
 
@@ -31,7 +38,8 @@ void *threader(void * arg){
     char buff[MAX_MSG_LEN];
     sprintf(buff,"%d",getpid());
     strcat(private_fifo,buff);
-    sprintf(buff,"%d",pthread_self());
+    strcat(private_fifo,".");
+    sprintf(buff,"%ld",pthread_self());
     strcat(private_fifo,buff);
 
     if (mkfifo(private_fifo,0660)<0)
@@ -49,65 +57,47 @@ void *threader(void * arg){
     printf("%s",server_msg);
     close(fd_dummy);
 
-    if (unlink(fifoName)<0)
-        printf("Error when destroying FIFO '%s'\n",fifoName);
+    if (unlink(private_fifo)<0)
+        printf("Error when destroying FIFO '%s'\n",private_fifo);
     else
-        printf("FIFO '%s' has been destroyed\n",fifoName);
+        printf("FIFO '%s' has been destroyed\n",private_fifo);
 
-
+    return NULL;
 }
 
-int main(int argc, char *argv[])
-{
-    int fd, fd_dummy;
-    char fifoName[MAX_MSG_LEN];
+int main(int argc, char *argv[]){
+    
+    pthread_t threads[NUM_THREADS_MAX];
+    int t=0;
+
 
     if (argc!=4) {
         printf("Usage: U1 <-t secs> fifoname\n");
         exit(1);
     }
     printf("Time of execution: %s\tFifoname:%s\n",argv[2],argv[3]);
-    time_t t;
-    srand((unsigned)time(&t));
 
-    fd=open(argv[3],O_WRONLY);
+    struct timeval begin,end;
+    gettimeofday(&begin,NULL); gettimeofday(&end,NULL);
+    double nsecs;
+    sscanf(argv[2],"%lf",&nsecs);
+    double time_taken=(end.tv_sec-begin.tv_sec)*1e6;
+    time_taken=(time_taken+(end.tv_usec-begin.tv_usec))*1e6;
 
-    if (fd == -1) {
-        printf("Oops !!! Service is closed !!!\n");
-        exit(1);
+    char *fifo_copy;
+    strcpy(fifo_copy,argv[3]);
+
+    while(time_taken<nsecs){
+        pthread_create(&threads[t],NULL,threader,&fifo_copy);
+        pthread_join(threads[t],NULL);
+        usleep(50000);
+        gettimeofday(&end,NULL);
+        time_taken=(end.tv_sec-begin.tv_sec)*1e6;
+        time_taken=(time_taken+(end.tv_usec-begin.tv_usec))*1e-6;
+        printf("Time ocurred: %f\n",time_taken);
+
     }
-
-    printf("FIFO 'requests' openned in WRITEONLY mode\n");
-
-    char msg[MAX_MSG_LEN];
-    sprintf(fifoName, "fifo_ans_%d",getpid());
-    sprintf(msg,"%d:%d:%s",atoi(argv[1]),atoi(argv[2]),fifoName);
-
-    write(fd,&msg,MAX_MSG_LEN);
-
-    close(fd);
-
-    if (mkfifo(fifoName,0660)<0)
-        if (errno == EEXIST) printf("FIFO '%s' already exists\n",fifoName);
-        else printf("Can't create FIFO\n");
-    else 
-        printf("FIFO '%s' sucessfully created\n",fifoName);
-    
-    if ((fd_dummy=open(fifoName,O_RDONLY)) !=-1)
-        printf("FIFO '%s' openned in READONLY mode\n",fifoName);
-
-    char server_msg[MAX_MSG_LEN];
-
-    read(fd_dummy,&server_msg,MAX_MSG_LEN);
-    printf("%s",server_msg);
-    close(fd_dummy);
-
-    if (unlink(fifoName)<0)
-        printf("Error when destroying FIFO '%s'\n",fifoName);
-    else
-        printf("FIFO '%s' has been destroyed\n",fifoName);
-    
-    */
+    printf("FInished work\n");
 
     return 0;
 } 
--- Projects/SecondProject/defs.h
+++ Projects/SecondProject/defs.h
@@ -1,3 +1,6 @@
+
+#define MAX_NAME_LEN 25
+
 struct cln_msg{
     char msg[MAX_NAME_LEN];
 };
